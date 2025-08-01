85f29e8a4e04847727f1905856d019de375885a7
unknown <pharishkumar123@gmail.com>
2025-08-01 14:56:06 +0530

    changes made for responsive to mobile

--- components/NavBarN.tsx
+++ components/NavBarN.tsx
@@ -102,11 +102,11 @@ export default function NavBarN() {
             >
               <span className="absolute -inset-1.5" />
               <span className="sr-only">View notifications</span>
-              <BellIcon aria-hidden="true" className="size-6" />
+              <BellIcon aria-hidden="true" className="size-6 hidden" />
             </button>
             <ModeToggle />
             {/* Profile dropdown */}
-            <Menu as="div" className="relative ml-3">
+            <Menu as="div" className="relative ml-3 hidden">
               <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-hidden focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-offset-2 focus-visible:ring-offset-gray-800">
                 <span className="absolute -inset-1.5" />
                 <span className="sr-only">Open user menu</span>
